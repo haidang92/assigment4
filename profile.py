e44512ab8097ef8bbbf6153605fa28d2a7c430b6
haidang92 <38142886+haidang92@users.noreply.github.com>
2018-10-06 21:56:52 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -32,28 +32,27 @@ tour = IG.Tour()
 tour.Description(IG.Tour.TEXT,tourDescription)
 request.addTour(tour)
 
-prefixForIP = "192.168.1."
 
 link = request.LAN("lan")
 
 for i in range(6):
   if i == 0:
     node = request.XenVM("head")
-    node.routable_control_ip = "true"
+    node.routable_control_ip = "true"   
   elif i == 1:
     node = request.XenVM("metadata")
   elif i == 2:
     node = request.XenVM("storage")
   else:
-    node = request.XenVM("compute-" + str(i-2))
-    node.cores = 4
+    node = request.XenVM("compute" + str(i-2))
+    node.cores = 2
     node.ram = 4096
     
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
   
-  iface = node.addInterface("if" + str(i))
+  iface = node.addInterface("if" + str(i-3))
   iface.component_id = "eth1"
-  iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
+  iface.addAddress(pg.IPv4Address("192.168.1." + str(i + 1), "255.255.255.0"))
   link.addInterface(iface)
   
   node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/passwordless.sh"))
@@ -61,11 +60,24 @@ for i in range(6):
   node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/install_mpi.sh"))
   node.addService(pg.Execute(shell="sh", command="sudo /local/repository/install_mpi.sh"))
   
-  # This code segment is added per Benjamin Walker's solution to address the StrictHostKeyCheck issue of ssh
   node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/ssh_setup.sh"))
   node.addService(pg.Execute(shell="sh", command="sudo -H -u DT882578 bash -c '/local/repository/ssh_setup.sh'"))
- 
+  
   node.addService(pg.Execute(shell="sh", command="sudo su DT882578 -c 'cp /local/repository/source/* /users/DT882578'"))
   
+  if i == 0 or i == 2:
+    node.addService(pg.Execute(shell="sh", command="yum -y install nfs-utils nfs-utils-lib"))
+    node.addService(pg.Execute(shell="sh", command="sudo /etc/init.d/portmap start"))
+    node.addService(pg.Execute(shell="sh", command="yum -y install portmap"))
+    node.addService(pg.Execute(shell="sh", command="sudo /etc/init.d/nfs start"))
+  
+  if i == 0:
+    node.addService(pg.Execute(shell="sh", command="mkdir /software"))
+  if i == 2:
+    node.addService(pg.Execute(shell="sh", command="mkdir /scratch"))
+  
+  if i == 3 or i == 4 or i == 5:
+    node.addService(pg.Execute(shell="sh", command="mkdir /scratch"))
+    
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
