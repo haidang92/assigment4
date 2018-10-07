7ff4028c0bc12dfcde3dc8bac9d5d102b0dca09b
haidang92 <38142886+haidang92@users.noreply.github.com>
2018-10-06 23:07:41 -0400

    Create mpi_path_setup.sh

---
+++ mpi_path_setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "export PATH='$PATH:/software/bin'" >> /users/DT882578/.bashrc
+echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/lib/'" >> /users/DT882578/.bashrc
+source /users/gb773994/.bashrc
