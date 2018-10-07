815a7eda9484a8afe4aabd0493f61056bd2acec5
haidang92 <38142886+haidang92@users.noreply.github.com>
2018-10-06 23:10:07 -0400

    Update mpi_path_setup.sh

--- mpi_path_setup.sh
+++ mpi_path_setup.sh
@@ -2,4 +2,5 @@
 
 echo "export PATH='$PATH:/software/bin'" >> /users/DT882578/.bashrc
 echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/lib/'" >> /users/DT882578/.bashrc
-source /users/gb773994/.bashrc
+
+source /users/DT882578/.bashrc
