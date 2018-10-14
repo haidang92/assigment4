cf100d5f02d62f66ecdd789e8cdf4dbff14d95c1
haidang92 <38142886+haidang92@users.noreply.github.com>
2018-10-14 18:13:38 -0400

    Delete mpi_path_setup.sh

--- mpi_path_setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "export PATH='$PATH:/software/bin'" >> /users/DT882578/.bashrc
-echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/lib/'" >> /users/DT882578/.bashrc
-
-source /users/DT882578/.bashrc
