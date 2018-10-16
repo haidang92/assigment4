bb9f1009e9fc756c708aab7431015c966f0e064a
haidang92 <38142886+haidang92@users.noreply.github.com>
2018-10-15 23:41:35 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -38,7 +38,6 @@ link = request.LAN("lan")
 
 for i in range(15):
   
-  
   if i == 0:
     node = request.XenVM("head")
     node.routable_control_ip = "true"
@@ -47,12 +46,11 @@ for i in range(15):
     node.addService(pg.Execute(shell="sh", command="sudo mkdir -m 755 /software"))
     node.addService(pg.Execute(shell="sh", command="sudo mkdir /scratch"))   
     #nfs service
-    node.addService(pg.Execute(shell="sh", command="sudo yum -y install nfs-utils"))
-    
     node.addService(pg.Execute(shell="sh", command="sudo systemctl enable nfs-server.service"))
     node.addService(pg.Execute(shell="sh", command="sudo systemctl start nfs-server.service"))
     # add MPI
-    
+    node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/scripts/install_mpi.sh"))
+    node.addService(pg.Execute(shell="sh", command="sudo /local/repository/scripts/install_mpi.sh"))
     
     node.addService(pg.Execute(shell="sh", command="sudo rm /etc/exports"))
     node.addService(pg.Execute(shell="sh", command="sudo cp /local/repository/exports_head /etc/exports"))
@@ -60,23 +58,20 @@ for i in range(15):
     node.addService(pg.Execute(shell="sh", command="sudo exportfs -a"))
     node.addService(pg.Execute(shell="sh", command="sleep 2m"))
     
-    
     # Mount
     node.addService(pg.Execute(shell="sh", command="sudo mount 192.168.1.3:/scratch /scratch"))
     node.addService(pg.Execute(shell="sh", command="sudo echo '192.168.1.3:/scratch /scratch nfs4 rw,relatime,vers=4.1,rsize=131072,wsize=131072,namlen=255,hard,proto=tcp,port=0,timeo=600,retrans=2,sec=sys,local_lock=none,addr=192.168.1.3,_netdev,x-systemd.automount 0 0' | sudo tee --append /etc/fstab"))
-    #node.addService(pg.Execute(shell="sh", command="sudo su nq666287 -c \"echo '192.168.1.3:/scratch /scratch nfs defaults 0 0' >> /etc/fstab\""))
-    node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/scripts/install_mpi.sh"))
-    node.addService(pg.Execute(shell="sh", command="sudo /local/repository/scripts/install_mpi.sh"))
+    
+    
   elif i == 1:
     node = request.XenVM("metadata")
     
+
   elif i == 2:
-    node = request.XenVM("storage")
-    
+    node = request.XenVM("storage")  
     node.addService(pg.Execute(shell="sh", command="sudo mkdir -m 755 /scratch"))
-    # nfs service
-    node.addService(pg.Execute(shell="sh", command="sudo yum -y install nfs-utils"))
     
+    # nfs service
     node.addService(pg.Execute(shell="sh", command="sudo systemctl enable nfs-server.service"))
     node.addService(pg.Execute(shell="sh", command="sudo systemctl start nfs-server.service"))
      
@@ -85,27 +80,10 @@ for i in range(15):
     node.addService(pg.Execute(shell="sh", command="sudo chmod 777 /etc/exports"))
     node.addService(pg.Execute(shell="sh", command="sudo exportfs -a"))
 
-  else:
-    node = request.XenVM("compute-" + str(i-2))
-    node.cores = 2
-    node.ram = 4096
-    node.addService(pg.Execute(shell="sh", command="sudo mkdir /scratch"))
-    node.addService(pg.Execute(shell="sh", command="sudo mkdir /software"))
-    node.addService(pg.Execute(shell="sh", command="sudo chmod 777 /scratch"))
-    node.addService(pg.Execute(shell="sh", command="sudo chmod 777 /software"))
     
-    node.addService(pg.Execute(shell="sh", command="sudo mount 192.168.1.3:/scratch /scratch"))
-    node.addService(pg.Execute(shell="sh", command="sudo su DT882578 -c \"echo '192.168.1.3:/scratch /scratch nfs defaults 0 0' >> /etc/fstab\""))
-
-    # Mount
-    node.addService(pg.Execute(shell="sh", command="sudo mount 192.168.1.1:/software /software"))
-    node.addService(pg.Execute(shell="sh", command="sudo su DT882578 -c \"echo '192.168.1.1:/software /software nfs defaults 0 0' >> /etc/fstab\""))
-
-    # Add MPI to mpi_path
-    node.addService(pg.Execute(shell="sh", command="sudo chmod 777 /local/repository/scripts/mpi_path_setup.sh"))
-    node.addService(pg.Execute(shell="sh", command="sudo -H -u DT882578 bash -c '/local/repository/scripts/mpi_path_setup.sh'"))
-  
+    
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+  
   iface = node.addInterface("if" + str(i))
   iface.component_id = "eth1"
   iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
@@ -117,15 +95,37 @@ for i in range(15):
   node.addService(pg.Execute(shell="sh", command="sudo -H -u DT882578 bash -c '/local/repository/ssh_setup.sh'"))
   node.addService(pg.Execute(shell="sh", command="sudo systemctl disable firewalld"))
   node.addService(pg.Execute(shell="sh", command="sudo su DT882578 -c 'cp /local/repository/source/* /users/DT882578'"))
+  
+  
+ else:
+   node = request.XenVM("compute-" + str(i-2))
+   node.cores = 2
+   node.ram = 4096
+   node.addService(pg.Execute(shell="sh", command="sudo mkdir /scratch"))
+   node.addService(pg.Execute(shell="sh", command="sudo mkdir /software"))
+   node.addService(pg.Execute(shell="sh", command="sudo chmod 777 /scratch"))
+   node.addService(pg.Execute(shell="sh", command="sudo chmod 777 /software"))
+    
+  # Mount 
+   node.addService(pg.Execute(shell="sh", command="sudo mount 192.168.1.3:/scratch /scratch"))
+   node.addService(pg.Execute(shell="sh", command="sudo su DT882578 -c \"echo '192.168.1.3:/scratch /scratch nfs defaults 0 0' >> /etc/fstab\""))
+
+    # Mount
+   node.addService(pg.Execute(shell="sh", command="sudo mount 192.168.1.1:/software /software"))
+   node.addService(pg.Execute(shell="sh", command="sudo su DT882578 -c \"echo '192.168.1.1:/software /software nfs defaults 0 0' >> /etc/fstab\""))
+
+    # Add MPI to mpi_path
+   node.addService(pg.Execute(shell="sh", command="sudo chmod 777 /local/repository/scripts/mpi_path_setup.sh"))
+   node.addService(pg.Execute(shell="sh", command="sudo -H -u DT882578 bash -c '/local/repository/scripts/mpi_path_setup.sh'"))
+      
+
+  
+ 
+    
+
+      
     
   
   
-  
-  
-   
-  
-
-  
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
-
